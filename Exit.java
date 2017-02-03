26f1eeb021819931ee5268ef6b8a966651e5405b
LordMilton <dufour.bradley@gmail.com>
2017-02-03 13:52:19 -0500

    "Version 1"

--- Exit.java
+++ Exit.java
@@ -8,5 +8,56 @@ package bork;
  * 
  */
 public class Exit {
-
+	String dir;
+	Room src;
+	Room dest;
+	
+	/**
+	 * 
+	 * @param dir Direction the Exit is accessed by
+	 * @param src Room the Exit is in
+	 * @param desc Room the Exit leads to
+	 */
+	public Exit(String dir, Room src, Room dest)
+	{
+		this.dir = dir;
+		this.src = src;
+		this.dest = dest;
+	}
+	
+	/**
+	 * 
+	 * @return Description including the direction the Exit leads, and the Room the Exit leads to
+	 */
+	String describe()
+	{
+		return ("You can go "+ dir +" to "+ dest);
+	}
+	
+	/**
+	 * 
+	 * @return Direction the Exit leads
+	 */
+	public String getDir()
+	{
+		return dir;
+	}
+	
+	/**
+	 * 
+	 * @return Room the Exit is within
+	 */
+	public Room getSrc()
+	{
+		return src;
+	}
+	
+	/**
+	 * 
+	 * @return Room the exit leads to
+	 */
+	public Room getDest()
+	{
+		return dest;
+	}
 }
--- Room.java
+++ Room.java
@@ -3,10 +3,93 @@
  */
 package bork;
 
+import java.util.ArrayList;
+
 /**
  * @author Bradley Dufour
  * 
  */
 public class Room {
-
+	private String title;
+	private String desc;
+	private ArrayList<Exit> exits;
+	private boolean beenHere;
+	
+	/**
+	 * 
+	 * @param title Name of the Room
+	 */
+	public Room(String title)
+	{
+		this.title = title;
+		exits = new ArrayList<Exit>(6);
+		desc = "";
+		beenHere = false;
+	}
+	
+	/**
+	 * 
+	 * @return Room's name
+	 */
+	public String getTitle()
+	{
+		return title;
+	}
+	
+	/**
+	 * 
+	 * @param desc Description of the Room (not exits or title)
+	 */
+	public void setDesc(String desc)
+	{
+		this.desc = desc;
+	}
+	
+	/**
+	 * 
+	 * @return Entire description of the Room with exits, title, and 'desc' if the player has not been
+	 * 		   in the Room before
+	 */
+	String describe()
+	{
+		String description = "";
+		description += title +"\n";
+		if(!beenHere)
+		{
+			description += desc;
+		}
+		for(Exit exit:exits)
+		{
+			description += (" "+ exit.describe());
+		}
+		
+		beenHere = true;
+		return description;
+	}
+	
+	/**
+	 * 
+	 * @param dir Direction to look for an Exit to leave by
+	 * @return Room user ends in (current Room if dir has no corresponding Exit)
+	 */
+	Room leaveBy(String dir)
+	{
+		for(Exit exit:exits)
+		{
+			if(exit.getDir().equals(dir))
+			{
+				return exit.getDest();
+			}
+		}
+		return this;
+	}
+	
+	/**
+	 * 
+	 * @param exit Exit to add to Room's list of Exits
+	 */
+	public void addExit(Exit exit)
+	{
+		exits.add(exit);
+	}
 }
